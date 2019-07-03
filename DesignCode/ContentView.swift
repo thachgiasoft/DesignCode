8cf98188ed381cedb0303b3840b7a2ece81dd161
Tom Hartnett <tomhartnett@gmail.com>
2019-07-03 07:08:19 -0500

    Completed section 2 of the course.

--- DesignCode/ContentView.swift
+++ DesignCode/ContentView.swift
@@ -10,20 +10,42 @@ import SwiftUI
 
 struct ContentView : View {
     var body: some View {
-        VStack {
+        ZStack {
+            
             VStack {
-                Text("UI Design")
-                    .font(.title)
-                    .fontWeight(.bold)
-                    .color(Color("accent"))
-                    .padding(.top)
-                Text(/*@START_MENU_TOKEN@*/"Certificate"/*@END_MENU_TOKEN@*/)
-                    .color(.white)
-            }
-            Image("Background")
-            }
-            .background(Color.black)
-            .cornerRadius(10)
+                Text("Card Back")
+                }
+                .frame(width: 300, height: 220.0)
+                .background(/*@START_MENU_TOKEN@*/Color.blue/*@END_MENU_TOKEN@*/)
+                .cornerRadius(10)
+                .shadow(radius: 20)
+                .offset(x: 0, y: -20)
+            
+            VStack {
+                HStack {
+                    VStack(alignment: .leading) {
+                        Text("UI Design")
+                            .font(.headline)
+                            .fontWeight(.bold)
+                            .color(Color("accent"))
+                            .padding(.top)
+                        Text(/*@START_MENU_TOKEN@*/"Certificate"/*@END_MENU_TOKEN@*/)
+                            .color(.white)
+                    }
+                    Spacer()
+                    Image("Logo")
+                        .resizable()
+                        .frame(width: 30.0, height: 30.0)
+                    }
+                    .padding(.horizontal)
+                Spacer()
+                Image("Background")
+                }
+                .frame(width: 340.0, height: 220.0)
+                .background(Color.black)
+                .cornerRadius(10)
+                .shadow(radius: 20)
+        }
     }
 }
 
