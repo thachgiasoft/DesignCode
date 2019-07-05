f445c9becf90af93222de72199653dc44ae4e6fa
Tom Hartnett <tomhartnett@gmail.com>
2019-07-05 08:04:45 -0500

    Completed section 9 of the course.  Also changed starting view to Home (not in tutorial).

--- DesignCode/Home.swift
+++ DesignCode/Home.swift
@@ -10,13 +10,25 @@ import SwiftUI
 
 struct Home : View {
     @State var show = false
+    @State var showProfile = false
     
     var body: some View {
         ZStack {
             
-            Button(action: { self.show.toggle() }) {
-                Text("Open Menu")
-            }
+            ContentView()
+                .background(Color.white)
+                .cornerRadius(30)
+                .shadow(radius: 20)
+                .animation(.fluidSpring())
+                .offset(y: showProfile ? 40 : UIScreen.main.bounds.height)
+            
+            MenuButton(show: $show)
+                .offset(x: -30, y: showProfile ? 0 : 80)
+                .animation(.fluidSpring())
+            
+            MenuRight(show: $showProfile)
+                .offset(x: -16, y: showProfile ? 0 : 88)
+                .animation(.fluidSpring())
             
             MenuView(show: $show)
         }
@@ -86,5 +98,57 @@ struct MenuView : View {
             .tapAction {
                 self.show.toggle()
             }
+    }
+}
+
+struct CircleButton : View {
+    var icon = "person.crop.circle"
+    var body: some View {
+        return HStack {
+            Image(systemName: icon)
+                .foregroundColor(.black)
+        }
+        .frame(width: 44, height: 44)
+        .background(Color.white)
+        .cornerRadius(30)
+        .shadow(color: Color("buttonShadow"), radius: 10, x: 0, y: 10)
+}
+}
+
+struct MenuButton : View {
+    @Binding var show: Bool
+    var body: some View {
+        return ZStack(alignment: .topLeading) {
+            Button(action: { self.show.toggle() }) {
+                HStack {
+                    Spacer()
+                    Image(systemName: "list.dash")
+                        .foregroundColor(.black)
+                }
+                .padding(.trailing)
+                .frame(width: 90, height: 60)
+                .background(Color.white)
+                .cornerRadius(30)
+                .shadow(color: Color("buttonShadow"), radius: 10, x: 0, y: 10)
+        }
+        Spacer()
+    }
+}
+}
+
+struct MenuRight : View {
+    @Binding var show: Bool
+    var body: some View {
+        return ZStack(alignment: .topTrailing) {
+            HStack {
+                Button(action: { self.show.toggle() }) {
+                    CircleButton(icon: "person.crop.circle")
+                }
+                Button(action: { self.show.toggle() }) {
+                    CircleButton(icon: "bell")
+                }
+            }
+            Spacer()
+        }
     }
 }
